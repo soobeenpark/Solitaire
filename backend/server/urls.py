18da7a41b5345ac8b42cf07dcc85a89b2b973fe9
Soobeen Park <soobeenpark.dev@gmail.com>
2020-11-11 21:58:06 +0900

    Change backend DB from sqlite to postgresql

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 
 urlpatterns = [
-    re_path(r'', include('api.urls')),
+    re_path(r"", include("api.urls")),
 ]
